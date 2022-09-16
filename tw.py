e8c369194062c263ee3ab2f52e0cf75017cc6686
sanggoo <sanggoo.git@gmail.com>
2022-09-16 22:39:39 +0900

    Update tw.py for Python 3

--- tw.py
+++ tw.py
@@ -14,9 +14,9 @@ def readITermConfig(query):
   if os.path.isfile(iTerm_CONFIG_PATH):
     try:
       config = biplist.readPlist(iTerm_CONFIG_PATH)
-      defaultBookmarkGUID = config.get('Default Bookmark Guid')
-      for item in config['New Bookmarks']:
-        profileName = item.get('Name')
+      defaultBookmarkGUID = config.get(b'Default Bookmark Guid')
+      for item in config[b'New Bookmarks']:
+        profileName = item.get(b'Name').decode('ascii')
         if (item.get('Guid') == defaultBookmarkGUID): continue
         if len(query) > 0 and profileName.lower().find(query.lower()) == -1: continue
         iE = ET.SubElement(rE, 'item', valid = 'yes', arg = profileName, autocomplete = profileName)
@@ -24,7 +24,8 @@ def readITermConfig(query):
         tE.text = 'New iTerm window'
         stE = ET.SubElement(iE, 'subtitle')
         stE.text = profileName
-    except:
+    except Exception as e:
+      # print(e)
       return None
     return rE
   else:
@@ -32,5 +33,5 @@ def readITermConfig(query):
 
 res = readITermConfig(sys.argv[1] if len(sys.argv) > 1 else '')
 if res is not None: 
-  print '<?xml version="1.0"?>'
-  print ET.tostring(res)
+  print('<?xml version="1.0"?>')
+  print(ET.tostring(res).decode('ascii'))
